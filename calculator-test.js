e92d6d68a31719e32aab052e6f61c525d80712e7
Mostafa Nassr <mostafa.nassr88@gmail.com>
2021-01-13 12:05:31 -0500

    added unit tests for calculateMonthlyPayment

--- calculator-test.js
+++ calculator-test.js
@@ -1,11 +1,11 @@
+describe('calculateMonthlyPayment tests', function() {
+  it('should calculate the monthly rate correctly', function () {
+    expect(calculateMonthlyPayment({amount: 10, years: 1, rate: 0.2})).toBe('0.93');
+    expect(calculateMonthlyPayment({amount: 100, years: 1, rate: 0.2})).toBe('9.26');
+    expect(calculateMonthlyPayment({amount: 1000, years: 1, rate: 0.2})).toBe('92.63');
+  });
 
-it('should calculate the monthly rate correctly', function () {
-  // ...
+  it("should return a result with 2 decimal places", function() {
+      expect(calculateMonthlyPayment({amount: 0, years: 1, rate: 0.2})).toBe('0.00');
+  });
 });
-
-
-it("should return a result with 2 decimal places", function() {
-  // ..
-});
-
-/// etc
